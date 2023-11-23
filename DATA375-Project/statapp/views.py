6398e4f389a224479c2b664dc6340acae4c219c4
colestokes5 <128331073+colestokes5@users.noreply.github.com>
2023-11-23 00:26:13 -0700

    Delete DATA375-Project directory

--- DATA375-Project/statapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def test(request):
-    return render(request, "index.html")
-
-def index(request):
-    templates = "index.html"
-    return render(request, templates)
